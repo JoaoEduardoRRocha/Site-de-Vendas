bb08a58b41e191e17bdff7f43d64c23fdc8832fe
Joao <joaoeduardorrocha@gmail.com>
2023-04-05 15:02:23 -0300

    index

---
+++ codigosRemovidos.js
@@ -0,0 +1,125 @@
+/*
+
+        <!-- LEGENDA -->
+        <legend>OFERTAS</legend>
+
+
+<li>
+                <div class="botaoDeTrocaDeCor"> 
+                    <button class="botaoParaMudarParaCorPreta"></button>
+                </div>  
+                <div class="caixaComAsCamisasDentro">
+                    <img class="camisetaPreta" src="img/camisetapreta.png" alt="Camiseta Preta" />
+                    <img hidden class="camisetaVermelha" src="img/camisetavermelha.png" alt="Camiseta Vermelha" />
+                    <label for="">Camiseta Básica</label>
+                    <label for="">R$10,00</label>
+                    <button class="">Comprar</button>
+                    <button class="botaoCompraOuCarrinho">+ Carrinho</button>
+                </div>
+
+            </li> 
+
+            <li>
+                <div class="botaoDeTrocaDeCor"> 
+                    <button class="botaoParaMudarParaCorPreta"></button>
+                </div>  
+                <div class="caixaComAsCamisasDentro">
+                    <img class="camisetaPreta" src="img/camisetapreta.png" alt="Camiseta Preta" />
+                    <img hidden class="camisetaVermelha" src="img/camisetavermelha.png" alt="Camiseta Vermelha" />
+                    <label for="">Camiseta Básica</label>
+                    <label for="">R$10,00</label>
+                </div>
+                <div class="botaoComprarCarrinho">
+                    <button class="">Comprar</button>
+                    <button class="botaoCompraOuCarrinho">+ Carrinho</button>  
+            </li>
+
+            <li>
+                <div class="botaoDeTrocaDeCor"> 
+                    <button class="botaoParaMudarParaCorPreta"></button>
+                </div>  
+                <div class="caixaComAsCamisasDentro">
+                    <img class="camisetaPreta" src="img/camisetapreta.png" alt="Camiseta Preta" />
+                    <img hidden class="camisetaVermelha" src="img/camisetavermelha.png" alt="Camiseta Vermelha" />
+                    <label for="">Camiseta Básica</label>
+                    <label for="">R$10,00</label>
+                </div>
+                <div class="botaoComprarCarrinho">
+                    <button class="">Comprar</button>
+                    <button class="botaoCompraOuCarrinho">+ Carrinho</button>  
+            </li> 
+
+
+                    <!-- SEGUNDA ÁREA DE COMPRAS DO SITE -->
+        <ul id="segundaAreaDeCompras">
+            <li>
+                <div class="imagemDePromocao"> 
+                    <img class="imgPromocao" src="/img/promoção.png" alt="promoção">
+                </div> 
+                <div class="caixaComAsCamisasDentro">
+                    <img src="img/camisetapreta.png" alt="Camiseta Preta" />
+                    <label for="">Camiseta Básica</label>
+                    <label for="">R$ 9,99</label>
+                </div>
+                <div class="botaoComprarCarrinho">
+                    <button class="botaoCompraOuCarrinho">Comprar</button>
+                    <button class="botaoCompraOuCarrinho">+ Carrinho</button> 
+            </li> 
+            <li>
+                <div class="imagemDePromocao"> 
+                    <img class="imgPromocao" src="/img/promoção.png" alt="promoção">
+                </div> 
+                <div class="caixaComAsCamisasDentro">
+                    <img src="img/camisetapreta.png" alt="Camiseta Preta" />
+                    <label for="">Camiseta Básica</label>
+                    <label for="">R$ 9,99</label>
+                </div>
+                <div class="botaoComprarCarrinho">
+                    <button class="">Comprar</button>
+                    <button class="botaoCompraOuCarrinho">+ Carrinho</button>
+                </div>   
+            </li>
+            <li>
+                <div class="imagemDePromocao"> 
+                    <img class="imgPromocao" src="/img/promoção.png" alt="promoção">
+                </div>  
+                <div class="caixaComAsCamisasDentro">
+                    <img src="img/camisetapreta.png" alt="Camiseta Preta" />
+                    <label for="">Camiseta Básica</label>
+                    <label for="">R$ 9,99</label>
+                </div>
+                <div class="botaoComprarCarrinho">
+                    <button class="">Comprar</button>
+                    <button class="botaoCompraOuCarrinho">+ Carrinho</button>
+                </div>     
+            </li>
+        </ul>
+
+
+
+        
+        botaoPreto.addEventListener('click', function() {
+    camisetaPretaParaVermelha.toggleAttribute('hidden')
+    camisetaVermelhaParaPreta.toggleAttribute('hidden')
+})
+
+
+/* TODOS OS BOTÕES DO SITE */
+/*.camisetaPreta {
+    display: none;
+}
+.camisetaPreta.open {
+    display: block;
+}
+
+.camisetaVermelha {
+    display: none;
+}
+
+.camisetaVermelha.open {
+    display: block;
+}
+*/
+
+
+
